bc251a17fa8364bcaed20d4ec8fe9f06ba786149
doma2k <79820904+doma2k@users.noreply.github.com>
2022-10-30 15:11:56 +0000

    Create portsEnable.sh

---
+++ portsEnable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+filename='ports.txt'
+ufw='ufw allow'
+
+sudo lsof -i -P -n | grep LISTEN | sed -n 's/.*:\($*\)/\1/p' | grep -Eo "\b[0-9]+\b" >> ports.txt
+
+while read p; do 
+    $ufw $p
+done < "$filename"
+
+rm ports.txt
+
+ufw enable 
